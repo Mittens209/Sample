8206bbd446e579407682e4baa73af7b078fb4667
Mittens1 <junnellemiguel09@gmail.com>
2025-09-25 13:35:28 +0800

    added some files

---
+++ chart1.py
@@ -0,0 +1,8 @@
+sum = 0
+counter = 1
+
+while counter <= 100:
+    sum = sum + counter
+    counter = counter + 1
+
+print(sum)
---
+++ chart2.py
@@ -0,0 +1,11 @@
+a = input("Input a: ")
+b = input("Input b: ")
+c = input("Input c: ")
+
+maximum = a
+if b > maximum:
+    maximum = b
+if c > maximum:
+    maximum = c
+
+print(maximum)
